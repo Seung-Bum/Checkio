02668a5f5df107cac88aa11d4f472018c965a1ac
Seung-Bum <sb910126@gmail.com>
2022-03-12 22:31:09 +0900

    'NumberLength.js'

---
+++ NumberLength.js
@@ -0,0 +1,9 @@
+// 글자수 새기
+
+function numberLength(number) {
+    return number.toString().length;
+}
+
+console.log('Example:');
+console.log(numberLength(10)); // 2
+console.log(numberLength(4)); // 1
