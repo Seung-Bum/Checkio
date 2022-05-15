b0ee9e05ad67d6a4c2a400c823ba49fdcfe3f017
Seung-Bum <sb910126@gmail.com>
2022-05-15 19:32:09 +0900

    IsEven.js complete

---
+++ Initiation/IsEven.js
@@ -0,0 +1,20 @@
+// isEven(2) == true
+// isEven(5) == false
+// isEven(0) == true
+
+
+function isEven(num) {
+    if (num % 2 == 0) {
+        // console.log(true)
+        return true
+    } else {
+        // console.log(false)
+        return false
+    }
+}
+
+
+isEven(2)
+isEven(5)
+isEven(0)
+
